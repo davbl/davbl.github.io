38af14d71a927da916a72433a27091f7e49e031e
akuataya <akuataja@gmail.com>
2024-10-29 17:51:31 +0100

    Implement Figma design for Biomedical section

--- src/components/biomed/biomed.jsx
+++ src/components/biomed/biomed.jsx
@@ -1,5 +1,11 @@
 import "./biomed.css";
 import { Squircle } from "corner-smoothing";
+import techniques from "./img/techniques-4x.png";
+import labs from "./img/labs-4x.png";
+import organisms from "./img/organisms-4x.png";
+import brain from "./img/brain-4x.png";
+import redCross from "./img/red-cross-4x.png";
+import award from "./img/award-4x.png";
 
 function Biomed() {
   return (
@@ -11,10 +17,31 @@ function Biomed() {
 
       <div className="bio-bento">
         <Squircle
-          cornerRadius={16}
+          className="techniques card"
+          cornerRadius={20}
           borderWidth={1.5}
-          className="card techniques">
-          <h4>Techniques</h4>
+          cornerSmoothing={0.8}>
+          <div className="card-header">
+            <Squircle
+              className="photo-outline orange-outline"
+              cornerRadius={20}
+              borderWidth={1.5}
+              cornerSmoothing={0.8}>
+              <Squircle
+                className="photo-squircle"
+                cornerRadius={16}
+                cornerSmoothing={0.8}>
+                <img
+                  src={techniques}
+                  alt="A photo of a scientist working with a microcentrifuge tube and a biopsy punch inside a biosafety cabinet."
+                  width="96"
+                  height="96"
+                />
+              </Squircle>
+            </Squircle>
+
+            <h4>Techniques</h4>
+          </div>
           <p>
             Gel electrophoresis, PCR, primer design, restriction digestion,
             restriction mapping, miniprep, bacterial transformation,
@@ -22,8 +49,32 @@ function Biomed() {
           </p>
         </Squircle>
 
-        <Squircle cornerRadius={16} borderWidth={1.5} className="card labs">
-          <h4>Laboratories</h4>
+        <Squircle
+          className="labs card"
+          cornerRadius={20}
+          borderWidth={1.5}
+          cornerSmoothing={0.8}>
+          <div className="card-header">
+            <Squircle
+              className="photo-outline orange-outline"
+              cornerRadius={20}
+              borderWidth={1.5}
+              cornerSmoothing={0.8}>
+              <Squircle
+                className="photo-squircle"
+                cornerRadius={16}
+                cornerSmoothing={0.8}>
+                <img
+                  src={labs}
+                  alt="A photo of a scientist in a laboratory examining a sample through a light microscope."
+                  width="96"
+                  height="96"
+                />
+              </Squircle>
+            </Squircle>
+
+            <h4>Laboratories</h4>
+          </div>
           <p>
             Timmons Lab, University of Kansas
             <br />
@@ -32,27 +83,106 @@ function Biomed() {
         </Squircle>
 
         <Squircle
-          cornerRadius={16}
+          className="organisms card"
+          cornerRadius={20}
           borderWidth={1.5}
-          className="card organisms">
-          <h4>Organisms</h4>
-          <p>E. coli, S. cerevisiae, C. elegans, HEK293</p>
-        </Squircle>
+          cornerSmoothing={0.8}>
+          <div className="card-header">
+            <Squircle
+              className="photo-outline orange-outline"
+              cornerRadius={20}
+              borderWidth={1.5}
+              cornerSmoothing={0.8}>
+              <Squircle
+                className="photo-squircle"
+                cornerRadius={16}
+                cornerSmoothing={0.8}>
+                <img
+                  src={organisms}
+                  alt="Microscopic view of a few C. elegans worms with visible internal structures on an agar plate."
+                  width="96"
+                  height="96"
+                />
+              </Squircle>
+            </Squircle>
 
-        <Squircle cornerRadius={16} borderWidth={1.5} className="card neuro">
-          <h4>Learn Neuroscience</h4>
+            <h4>Organisms</h4>
+          </div>
           <p>
-            A collection of resources for learning about the brain, nervous
-            system, and neurotechnology. Books, online courses, videos,
-            subreddits, twitter accounts, podcasts, and newsletters.
+            <span>E.&nbsp;coli, S.&nbsp;cerevisiae, C.&nbsp;elegans,</span>{" "}
+            HEK293
           </p>
         </Squircle>
 
         <Squircle
-          cornerRadius={16}
+          className="neuro card"
+          cornerRadius={20}
           borderWidth={1.5}
-          className="card red-cross">
-          <h4>American Red Cross</h4>
+          cornerSmoothing={0.8}>
+          <div className="card-header">
+            <Squircle
+              className="photo-outline neuro-outline"
+              cornerRadius={20}
+              borderWidth={1.5}
+              cornerSmoothing={0.8}>
+              <Squircle
+                className="photo-squircle"
+                cornerRadius={16}
+                cornerSmoothing={0.8}>
+                <img
+                  src={brain}
+                  alt="A photo of several coronal brain slices showing cortical and subcortical structures."
+                  width="96"
+                  height="96"
+                />
+              </Squircle>
+            </Squircle>
+
+            <h4>
+              Learn <br />
+              Neuroscience
+            </h4>
+          </div>
+
+          <p>
+            A collection of{" "}
+            <a
+              href="https://dvbl.notion.site/Learn-Neuroscience-66dc61f4f4de4003874f3aa9d27846a5"
+              target="_blank">
+              resources
+            </a>{" "}
+            for learning about the brain, nervous system, and neurotechnology.
+            Books, online courses, videos, subreddits, twitter accounts,
+            podcasts, and newsletters.
+          </p>
+        </Squircle>
+
+        <Squircle
+          className="red-cross card"
+          cornerRadius={20}
+          borderWidth={1.5}
+          cornerSmoothing={0.8}>
+          <div className="card-header">
+            <Squircle
+              className="photo-outline red-outline"
+              cornerRadius={20}
+              borderWidth={1.5}
+              cornerSmoothing={0.8}>
+              <Squircle
+                className="photo-squircle"
+                cornerRadius={16}
+                cornerSmoothing={0.8}>
+                <img
+                  src={redCross}
+                  alt="A photo of an American Red Cross volunteer wearing a vest covered in handwritten thank-you messages and signatures on the back."
+                  width="96"
+                  height="96"
+                />
+              </Squircle>
+            </Squircle>
+
+            <h4>American Red Cross</h4>
+          </div>
           <p>
             Our team of ~10 volunteers organized Community Blood Drives, made
             reusable pads for Tanzanian girls and women, and carried out a Home
@@ -60,8 +190,32 @@ function Biomed() {
           </p>
         </Squircle>
 
-        <Squircle cornerRadius={16} borderWidth={1.5} className="card award">
-          <h4>Mark L. Morris, Jr. Award</h4>
+        <Squircle
+          className="award card"
+          cornerRadius={20}
+          borderWidth={1.5}
+          cornerSmoothing={0.8}>
+          <div className="card-header">
+            <Squircle
+              className="photo-outline red-outline"
+              cornerRadius={20}
+              borderWidth={1.5}
+              cornerSmoothing={0.8}>
+              <Squircle
+                className="photo-squircle"
+                cornerRadius={16}
+                cornerSmoothing={0.8}>
+                <img
+                  src={award}
+                  alt="A photo of a realistic hard plastic heart model showing major arteries and veins."
+                  width="96"
+                  height="96"
+                />
+              </Squircle>
+            </Squircle>
+
+            <h4>Mark L. Morris, Jr. Award</h4>
+          </div>
           <p>
             My award-winning project for an Entrepreneurship class (2015) was a
             business plan for a fictional company offering automated EKG

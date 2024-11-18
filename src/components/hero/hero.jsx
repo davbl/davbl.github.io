30ad58deecbd97fe55c1b82ad504b13309ccd6fc
akuataya <akuataja@gmail.com>
2024-11-18 12:14:42 +0100

    Improve small hero text

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,4 +1,6 @@
 import "./hero.css";
+import { ScrollText } from "lucide-react";
+// import { BadgeInfo } from "lucide-react";
 
 function Hero() {
   return (
@@ -9,19 +11,23 @@ function Hero() {
       </h1>
 
       <p>
-        Hi, I’m Dave and I make software tools for biology and healthcare. I
-        have senior experience in design, junior in software development, and
-        training in molecular biology.
+        Hi, I’m Dave – I design and develop software tools for biology and
+        healthcare. I’m trained in molecular biology and have experience with
+        small-scale bioscience fundraising.
       </p>
 
-      <div className="hero-details">
-        <button>Origin story</button>
+      <div className="btn-and-fine-print">
+        <button>
+          <ScrollText size={15} strokeWidth={2} />
+          Origin story
+        </button>
 
         <div className="fine-print">
-          <span>☉</span>
+          {/* <span>☉</span> */}
+          {/* <BadgeInfo size={15} strokeWidth={2} className="icon" /> */}
           <p>
             Fine print: Only projects related to bio & health are showcased on
-            this site.
+            the site. Most have been of lower complexity so far.
           </p>
         </div>
       </div>

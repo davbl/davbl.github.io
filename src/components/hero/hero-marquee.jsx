2366db4a52571fd01cf7c08f2163f74b80577463
akuataya <akuataja@gmail.com>
2024-11-22 14:06:15 +0100

    Add marquee images

--- src/components/hero/hero-marquee.jsx
+++ src/components/hero/hero-marquee.jsx
@@ -1,4 +1,19 @@
 import "./hero-marquee.css";
+// Images - col1
+import Fw1 from "./img/flywire1.png";
+import Fw2 from "./img/flywire2.png";
+import Fw3 from "./img/flywire3.png";
+import Fw4 from "./img/flywire4.png";
+// Images - col2
+import hand from "./img/xray-hand.png";
+import hip from "./img/xray-hip.png";
+import angiogram from "./img/angiogram.png";
+import brain from "./img/brain.png";
+// Images - col2
+import prot1 from "./img/prot1.png";
+import prot2 from "./img/prot2.png";
+import prot3 from "./img/prot3.png";
+import prot4 from "./img/prot4.png";
 
 function HeroMarquee() {
   return (
@@ -7,18 +22,34 @@ function HeroMarquee() {
       <div className="marquee-column">
         {/* Track 1 */}
         <div className="marquee-track">
-          <div className="marquee-rectangle">1</div>
-          <div className="marquee-rectangle">2</div>
-          <div className="marquee-rectangle">3</div>
-          <div className="marquee-rectangle">4</div>
+          <div className="marquee-rectangle">
+            <img src={Fw1} alt="" className="img" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={Fw3} alt="" className="img" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={Fw2} alt="" className="img" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={Fw4} alt="" className="img" />
+          </div>
         </div>
 
         {/* Track 2 */}
         <div className="marquee-track">
-          <div className="marquee-rectangle">1</div>
-          <div className="marquee-rectangle">2</div>
-          <div className="marquee-rectangle">3</div>
-          <div className="marquee-rectangle">4</div>
+          <div className="marquee-rectangle">
+            <img src={Fw1} alt="" className="img" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={Fw3} alt="" className="img" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={Fw2} alt="" className="img" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={Fw4} alt="" className="img" />
+          </div>
         </div>
         {/* Column 1 end */}
       </div>
@@ -27,18 +58,34 @@ function HeroMarquee() {
       <div className="marquee-column">
         {/* Track 1 */}
         <div className="marquee-track reverse">
-          <div className="marquee-rectangle">5</div>
-          <div className="marquee-rectangle">6</div>
-          <div className="marquee-rectangle">7</div>
-          <div className="marquee-rectangle">8</div>
+          <div className="marquee-rectangle">
+            <img src={hand} alt="" className="hand" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={brain} alt="" className="brain" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={hip} alt="" className="hip" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={angiogram} alt="" className="angiogram" />
+          </div>
         </div>
 
         {/* Track 2 */}
         <div className="marquee-track reverse">
-          <div className="marquee-rectangle">5</div>
-          <div className="marquee-rectangle">6</div>
-          <div className="marquee-rectangle">7</div>
-          <div className="marquee-rectangle">8</div>
+          <div className="marquee-rectangle">
+            <img src={hand} alt="" className="hand" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={brain} alt="" className="brain" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={hip} alt="" className="hip" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={angiogram} alt="" className="angiogram" />
+          </div>
         </div>
         {/* Column 2 end */}
       </div>
@@ -47,18 +94,34 @@ function HeroMarquee() {
       <div className="marquee-column ">
         {/* Track 1 */}
         <div className="marquee-track">
-          <div className="marquee-rectangle">9</div>
-          <div className="marquee-rectangle">10</div>
-          <div className="marquee-rectangle">11</div>
-          <div className="marquee-rectangle">12</div>
+          <div className="marquee-rectangle">
+            <img src={prot1} alt="" className="protein" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={prot2} alt="" className="protein" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={prot3} alt="" className="protein" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={prot4} alt="" className="protein" />
+          </div>
         </div>
 
         {/* Track 2 */}
         <div className="marquee-track">
-          <div className="marquee-rectangle">9</div>
-          <div className="marquee-rectangle">10</div>
-          <div className="marquee-rectangle">11</div>
-          <div className="marquee-rectangle">12</div>
+          <div className="marquee-rectangle">
+            <img src={prot1} alt="" className="protein" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={prot2} alt="" className="protein" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={prot3} alt="" className="protein" />
+          </div>
+          <div className="marquee-rectangle">
+            <img src={prot4} alt="" className="protein" />
+          </div>
         </div>
         {/* Column 3 end */}
       </div>

1d44a663cbaa21bcf0466cc0de8a8eb9913a179f
akuataya <akuataja@gmail.com>
2024-11-22 14:03:48 +0100

    Lift header above marquee fade out

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header>
       <h3>Acid Nucleique</h3>
-      <h3>My online phenotype</h3>
+      <h3 className="right-align">My online phenotype</h3>
     </header>
   );
 }

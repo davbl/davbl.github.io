a834349c99211b0e9ecde5ec9b96869667fab6dd
akuataya <akuataja@gmail.com>
2024-11-27 19:35:29 +0100

    Adjust for mobile

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -7,33 +7,45 @@ import HeroText from "../../assets/hero-text.svg";
 
 function Hero() {
   return (
-    <section className="hero">
+    <section className="hero-section">
+      {/* Bg marquee */}
       <HeroMarquee />
+      <div className="hero-mobile">
+        {/* Hero svg */}
+        <h1>
+          <img src={HeroText} alt="Software & Biology" />
+        </h1>
 
-      <h1>
-        <img src={HeroText} alt="Software & Biology" />
-      </h1>
+        {/* Bigger text */}
+        <div className="bigger-text">
+          <p>
+            Hi, I’m Dave – I design and develop software tools for biology and
+            healthcare.
+          </p>{" "}
+          {/* Second paragraph */}
+          <p>
+            I’m trained in molecular biology and have experience with
+            small-scale{" "}
+            <Link to="/fundraising" className="link">
+              bioscience&nbsp;fundraising
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
 
-      <p>
-        Hi, I’m Dave – I design and develop software tools for biology and
-        healthcare. I’m trained in molecular biology and have experience with
-        small-scale{" "}
-        <Link to="/fundraising" className="link">
-          bioscience fundraising
-        </Link>
-        .
-      </p>
-
-      <div className="btn-and-fine-print">
-        <Link to="/origin" className="fine-print story">
+      {/* Small text */}
+      <div className="small-text-container">
+        {/* Origin story */}
+        <Link to="/origin" className="small-text story">
           <ScrollText size={14} strokeWidth={2} />
           <p>Origin story</p>
         </Link>
 
-        <div className="fine-print">
-          {/* <span>☉</span> */}
-          <BadgeInfo size={14} strokeWidth={2} className="icon" />
+        {/* Fine print */}
+        <div className="small-text">
           <p>
+            <BadgeInfo size={14} strokeWidth={2} className="icon" />
             Fine print: Only projects related to bio & health are showcased on
             the site.
           </p>

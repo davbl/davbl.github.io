3b56c16963e0717c29960d99f7f13af8a3c291b4
akuataya <akuataja@gmail.com>
2025-01-01 18:41:03 +0100

    Fix inconsistent marquee behavior in different browsers

--- src/components/logo-marquee/logo-marquee.jsx
+++ src/components/logo-marquee/logo-marquee.jsx
@@ -1,3 +1,5 @@
+import Marquee from "react-fast-marquee";
+import { useMediaQuery } from "react-responsive";
 import "./logo-marquee.css";
 
 // Images
@@ -10,31 +12,39 @@ import abyky from "./img/abyky-logo.svg";
 import progress from "./img/progress-forum-logo.svg";
 
 function Logos() {
+  // Detect if user prefers reduced motion
+  const prefersReducedMotion = useMediaQuery({
+    query: "(prefers-reduced-motion: reduce)",
+  });
+
+  // Define breakpoints for responsive gradientWidth
+  const isSmallScreen = useMediaQuery({ query: "(max-width: 650px)" });
+
+  // Determine gradientWidth based on screen size
+  let gradientWidth = 150; // Default for large screens
+  if (isSmallScreen) {
+    gradientWidth = 75;
+  }
+
   return (
     <section className="logos-section">
       <h3 className="subheading">Pleasure to have worked with</h3>
 
-      <div className="logos-container">
-        <div className="marquee">
-          <img src={mycroft} alt="Mycroft AI logo" loading="lazy" />
-          <img src={ntx} alt="NeurotechX logo" width="178" loading="lazy" />
-          <img src={drverde} alt="Doctor Verde logo" loading="lazy" />
-          <img src={bola} alt="Bola AI logo" loading="lazy" />
-          <img src={kisk} alt="KISK logo" loading="lazy" />
-          <img src={abyky} alt="Abyky logo" loading="lazy" />
-          <img src={progress} alt="Progress Forum logo" loading="lazy" />
-        </div>
-
-        <div className="marquee">
-          <img src={mycroft} alt="Mycroft AI logo" loading="lazy" />
-          <img src={ntx} alt="NeurotechX logo" width="178" loading="lazy" />
-          <img src={drverde} alt="Doctor Verde logo" loading="lazy" />
-          <img src={bola} alt="Bola AI logo" loading="lazy" />
-          <img src={kisk} alt="KISK logo" loading="lazy" />
-          <img src={abyky} alt="Abyky logo" loading="lazy" />
-          <img src={progress} alt="Progress Forum logo" loading="lazy" />
-        </div>
-      </div>
+      <Marquee
+        className="logo-track"
+        speed={40}
+        gradient={true}
+        gradientColor={"#2d3040"}
+        gradientWidth={gradientWidth}
+        play={!prefersReducedMotion}>
+        <img src={mycroft} alt="Mycroft AI logo" loading="lazy" />
+        <img src={ntx} alt="NeurotechX logo" width="178" loading="lazy" />
+        <img src={drverde} alt="Doctor Verde logo" loading="lazy" />
+        <img src={bola} alt="Bola AI logo" loading="lazy" />
+        <img src={kisk} alt="KISK logo" loading="lazy" />
+        <img src={abyky} alt="Abyky logo" loading="lazy" />
+        <img src={progress} alt="Progress Forum logo" loading="lazy" />
+      </Marquee>
     </section>
   );
 }
